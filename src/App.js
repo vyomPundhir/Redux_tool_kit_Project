3711931413be483ed509f08f467ccbc520bd4cb7
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-03-14 23:59:44 +0530

    project partailly completed

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       </div>
       <div className="space-x-6">
         <button
-        className="">
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
           Increment By 1
         </button>
 
